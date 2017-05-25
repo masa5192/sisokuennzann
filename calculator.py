ed3d49023fc84e666e7c0db9dd11f27d4f73b4ce
masa5192 <nino.road52@gmail.com>
2017-05-26 02:25:16 +0900

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,213 @@
+input_count1=input('')
+number1=int(input_count1)
+
+input_count2=input('')
+enz=input_count2
+
+input_count3=input('')
+number2=int(input_count3)
+
+input_count4=input('')
+number4=input_count4
+
+if enz== '+':
+    ans=number1+number2
+
+    if number4=='=':
+        print(ans)
+
+    while (number4 =='+'):
+        input_count5=input('')
+        number5=int(input_count5)
+
+        input_count6=input('')
+        number6=input_count6
+
+        ans+=number5
+        print(ans)
+
+    while (number4 =='-'):
+        input_count5=input('')
+        number5=int(input_count5)
+
+        input_count6=input('')
+        number6=input_count6
+
+        ans-=number5
+        print(ans)
+
+    while (number4 =='*'):
+        input_count5=input('')
+        number5=int(input_count5)
+
+        input_count4=input('')
+        number6=input_count6
+
+        ans*=number5
+        print(ans)
+
+    while (number4 =='/'):
+        input_count5=input('')
+        number5=int(input_count5)
+
+        input_count6=input('')
+        number6=input_count6
+
+        ans/=number5
+        print(ans)
+    
+
+
+elif enz=='-':
+    ans=number1 -number2
+    if number4=='=':
+        print(ans)
+
+    while (number4 =='+'):
+        input_count5=input('')
+        number5=int(input_count5)
+
+        input_count6=input('')
+        number6=input_count6
+
+        ans+=number5
+        print(ans)
+
+    while (number4 =='-'):
+        input_count5=input('')
+        number5=int(input_count5)
+
+        input_count6=input('')
+        number6=input_count6
+
+        ans-=number5
+        print(ans)
+
+    while (number4 =='*'):
+        input_count5=input('')
+        number5=int(input_count5)
+
+        input_count4=input('')
+        number6=input_count6
+
+        ans*=number5
+        print(ans)
+
+    while (number4 =='/'):
+        input_count5=input('')
+        number5=int(input_count5)
+
+        input_count6=input('')
+        number6=input_count6
+
+        ans/=number5
+        print(ans)
+    
+    
+
+elif enz=='/':
+    while (number2==0):
+        print('error')
+    ans =number1/number2
+    
+    if number4=='=':
+        print(ans)
+
+    while (number4 =='+'):
+        input_count5=input('')
+        number5=int(input_count5)
+
+        input_count6=input('')
+        number6=input_count6
+
+        ans+=number5
+        print(ans)
+
+    while (number4 =='-'):
+        input_count5=input('')
+        number5=int(input_count5)
+
+        input_count6=input('')
+        number6=input_count6
+
+        ans-=number5
+        print(ans)
+
+    while (number4 =='*'):
+        input_count5=input('')
+        number5=int(input_count5)
+
+        input_count4=input('')
+        number6=input_count6
+
+        ans*=number5
+        print(ans)
+
+    while (number4 =='/'):
+        input_count5=input('')
+        number5=int(input_count5)
+
+        input_count6=input('')
+        number6=input_count6
+
+        ans/=number5
+        print(ans)
+    
+
+elif enz=='*':
+    ans=number1*number2
+    
+    if number4=='=':
+            print(ans)
+
+    while (number4 =='+'):
+        input_count5=input('')
+        number5=int(input_count5)
+
+        input_count6=input('')
+        number6=input_count6
+
+        ans+=number5
+        print(ans)
+
+    while (number4 =='-'):
+        input_count5=input('')
+        number5=int(input_count5)
+
+        input_count6=input('')
+        number6=input_count6
+
+        ans-=number5
+        print(ans)
+
+    while (number4 =='*'):
+        input_count5=input('')
+        number5=int(input_count5)
+
+        input_count4=input('')
+        number6=input_count6
+
+        ans*=number5
+        print(ans)
+
+    while (number4 =='/'):
+        input_count5=input('')
+        number5=int(input_count5)
+
+        input_count6=input('')
+        number6=input_count6
+
+        ans/=number5
+        print(ans)
+    
+
+
+
+
+
+
+
+
+    
+   
+
